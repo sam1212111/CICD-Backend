9e9e7a86e2eee85aeb48179c2ca3371d6c3bd2c9
sam1212111 <sanatkumarbhowmik47@gmail.com>
2025-09-18 17:57:24 +0530

    Backend ready for Heroku

---
+++ app.py
@@ -0,0 +1,24 @@
+import os
+from flask import Flask, request, jsonify
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app)
+
+tasks = []
+
+@app.route('/tasks', methods=['GET'])
+def get_tasks():
+    return jsonify(tasks)
+
+@app.route('/tasks', methods=['POST'])
+def add_task():
+    data = request.get_json()
+    task = data.get('task')
+    if task:
+        tasks.append(task)
+    return jsonify({"success": True})
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
